49f327891df098717984a45883463c2efd23523d
romanthegg <rzadorskyy3454@stu.d214.org>
2020-12-03 16:19:22 -0500

    added label, button ,and textfield. when put the text in it works

--- GitHubTesting/ViewController.swift
+++ GitHubTesting/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        let data = text.text!
+        label.text = data
+    }
+    
 }
 
